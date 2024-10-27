7b124138daf14280e43f0fc64f513b6005aa6f85
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-10-26 22:21:48 -0500

    fix: clear logs added
    - Print network variables and their possible values
    - Print query examples
    - More explanatory printout of the inference process

--- bayesian_inference.py
+++ bayesian_inference.py
@@ -1,5 +1,8 @@
 import json
 
+# Variables globales para almacenar la normalizing constant
+normalizing_constant = 0
+
 # Función para cargar la red bayesiana desde archivo
 def load_bayesian_network(file_path):
     with open(file_path, 'r') as f:
@@ -12,16 +15,17 @@ def calculate_joint_probability(network, event):
     for node, value in event.items():
         if node in network['dependencies']:
             parent_values = ','.join([event[parent] for parent in network['dependencies'][node]])
-            key = f"{node}|{','.join(network['dependencies'][node])}"
-            print(f"Intentando acceder a la clave: {key} -> {parent_values}")
+            key = f"{node} | {','.join(network['dependencies'][node])}"
+            # print(f"If: Intentando acceder a la clave: {key} -> {parent_values}")
             if parent_values in network['probabilities'][key]:
                 prob = network['probabilities'][key][parent_values][value]
+                print(f"P({node} = {value} | {','.join(network['dependencies'][node])} = {parent_values}) = {prob}")
             else:
-                print(f"Combinación {parent_values} no encontrada en las probabilidades.")
+                print(f"P({node} = {value} | {','.join(network['dependencies'][node])} = {parent_values}) = NOT Found in Network")
                 return 0
         else:
-            print(f"Intentando acceder a la clave: {node} -> {value}")
             prob = network['probabilities'][node][value]
+            print(f"P({node} = {value}) = {prob}")
         joint_prob *= prob
     return joint_prob
 
@@ -32,6 +36,7 @@ def bayesian_inference(network, query, evidence):
     def recursive_inference(vars, evidence, query):
         if not vars:
             event = {**evidence, **query}
+            print()
             return calculate_joint_probability(network, event)
         var = vars[0]
         total_prob = 0
@@ -40,8 +45,19 @@ def bayesian_inference(network, query, evidence):
             total_prob += recursive_inference(vars[1:], new_evidence, query)
         return total_prob
 
-    query_prob = recursive_inference(hidden_vars, evidence, query)
-    normalizing_constant = recursive_inference(hidden_vars, evidence, {})
+    # Si la variable global de la constante de normalización es 0, calcularla
+    global normalizing_constant
+    if normalizing_constant == 0:
+        print("-------------------------------------------------------------------------------------")
+        print("Calculando la constante de normalización:")
+        normalizing_constant = recursive_inference(hidden_vars, evidence, {})
+        print(f"Constante de normalización = {normalizing_constant}")
+    
+    # Calcular la probabilidad de la query
+    print("-------------------------------------------------------------------------------------")
+    print(f"Calculando la probabilidad (sin normalizar) de {query} dado {evidence}:")
+    query_prob = recursive_inference(hidden_vars, evidence, query)    
+    print(f"P({query} | {evidence}) = {query_prob:.4f}")
     
     return query_prob / normalizing_constant
 
@@ -60,6 +76,27 @@ def main():
     file_path = input("Ingrese el nombre del archivo JSON con la red bayesiana: ")
     network = load_bayesian_network(file_path)
 
+    # Imprimir las variables de la red y sus posibles valores
+    print("------------------------------------------")
+    print("Variables de la red bayesiana:")
+    for node, data in network['nodes'].items():
+        print(f"{node}: {data['values']}")
+    print("------------------------------------------")
+
+    # Imprimir posibles queries recorriendo queries de la red que tienen este formato
+    # {
+    #     "Opcion": "1 (Probabilidad)",
+    #     "Variable": "Radiografia anormal",
+    #     "Valor esperado": "Si",
+    #     "Evidencia": "Fumador=Si"
+    #   }
+    print("Ejemplos de queries:")
+    for query in network['queries']:
+        print()
+        for key, value in query.items():
+            print(f"{key}: {value}")
+    print("------------------------------------------")
+
     # Pedir tipo de pregunta
     print("Seleccione el tipo de pregunta:")
     print("1. Probabilidad de un evento específico")
@@ -68,24 +105,35 @@ def main():
 
     if question_type == "1":
         # Pedir evento específico y evidencia
-        variable = input("Ingrese la variable para la consulta (ej: Appointment): ")
-        value = input(f"Ingrese el valor esperado para {variable} (ej: attend): ")
-        evidence_input = input("Ingrese la evidencia en formato 'variable1=valor1,variable2=valor2' (ej: Rain=light,Train=delayed): ")
-        evidence = dict(item.split("=") for item in evidence_input.split(","))
+        variable = input("Ingrese la variable para la consulta: ")
+        value = input(f"Ingrese el valor esperado para {variable}: ")
+        evidence_input = input("Ingrese la evidencia en formato 'variable1=valor1,variable2=valor2': ")
+        print()
+        if evidence_input == "":
+            evidence = {}
+        else:
+            evidence = dict(item.split("=") for item in evidence_input.split(","))
         query = {variable: value}
 
         # Realizar la inferencia
         prob = bayesian_inference(network, query, evidence)
+        print("=====================================================================================")
+        print("Resultado:")
         print(f"P({query} | {evidence}) = {prob:.4f}")
 
     elif question_type == "2":
         # Pedir variable y evidencia
-        variable = input("Ingrese la variable para la distribución (ej: Appointment): ")
-        evidence_input = input("Ingrese la evidencia en formato 'variable1=valor1,variable2=valor2' (ej: Rain=light,Train=delayed): ")
-        evidence = dict(item.split("=") for item in evidence_input.split(","))
+        variable = input("Ingrese la variable para la distribución: ")
+        evidence_input = input("Ingrese la evidencia en formato 'variable1=valor1,variable2=valor2': ")
+        print()
+        if evidence_input == "":
+            evidence = {}
+        else:
+            evidence = dict(item.split("=") for item in evidence_input.split(","))
 
         # Obtener la distribución
         dist = distribution_of_variable(network, variable, evidence)
+        print("=====================================================================================")
         print(f"Distribución de {variable} dado {evidence}:")
         for value, prob in dist.items():
             print(f"P({variable} = {value} | {evidence}) = {prob:.4f}")
